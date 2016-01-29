6521b5000a2c818a3914095c1233e38517e35307
sumanmaity112 <suman.maity112@gmail.com>
2016-01-29 16:37:39 +0530

    Created index page to track who are using this site

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var fs = require('fs');
 var lodash = require('lodash');
+var moment = require('moment-timezone');
+var bodyParser = require('body-parser');
 var updateDetails = require('./takeImportantDetails.js');
 var users={};
 
@@ -18,11 +21,13 @@ setInterval(function(){
 var IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP;
 var PORT = process.env.OPENSHIFT_NODEJS_PORT || 4040;
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine','jade');
+app.use(express.static('./HTML'));
 app.get('/', function(req, res){
-  res.render('allDetails',{user:Object.keys(users).sort()});
+	res.redirect('/index.html');
 });
+
 app.get(/\/interns/,function(req,res){
 	var userName = req.url.split('/interns/')[1];
 	userName = userName.replace(/%20/g,' ');
@@ -36,6 +41,11 @@ app.get(/\/interns/,function(req,res){
 		res.statusCode=405;
 		res.end('Invalid intern name')
 	}
+})
+
+app.post('/login',function(req,res){
+	fs.appendFile('./data/usersLog.log',req.body.name+'  '+moment(new Date().toISOString()).tz('Asia/Kolkata').format('DD-MM-YYYY hh:mma')+'\n',function(){});
+	res.render('allDetails',{user:Object.keys(users).sort()});
 })
 
 var server = http.createServer(app);
